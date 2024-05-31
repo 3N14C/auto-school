d654ea5fa087787057720f8bcad04f9145ef4ce0
3N14C <3n14c@vk.com>
2024-05-31 16:32:12 +0800

    Update price-section.tsx

--- src/app/(home)/_components/price-section.tsx
+++ src/app/(home)/_components/price-section.tsx
@@ -1,25 +1,25 @@
-"use client";
+// "use client";
 
-import { getAllCategories } from "@/actions/category/get-all";
-import { PriceCard } from "@/components/ui/price-card/price-card";
-import { cn } from "@/lib/utils";
-import { useQuery } from "@tanstack/react-query";
-import { FC } from "react";
+// import { getAllCategories } from "@/actions/category/get-all";
+// import { PriceCard } from "@/components/ui/price-card/price-card";
+// import { cn } from "@/lib/utils";
+// import { useQuery } from "@tanstack/react-query";
+// import { FC } from "react";
 
-export const PriceSection: FC = () => {
-  const { data: categories, isLoading } = useQuery({
-    queryKey: ["all-categories"],
-    queryFn: getAllCategories,
-  });
+// export const PriceSection: FC = () => {
+//   const { data: categories, isLoading } = useQuery({
+//     queryKey: ["all-categories"],
+//     queryFn: getAllCategories,
+//   });
 
-  return (
-    <div
-      className={cn("grid lg:gap-0 gap-5 items-center justify-items-center", {
-        "lg:grid-cols-2": categories && categories?.length > 1,
-        "w-1/2 mx-auto": categories && categories.length === 1,
-      })}
-    >
-      {categories?.map((card) => <PriceCard key={card.id} card={card} />)}
-    </div>
-  );
-};
+//   return (
+//     <div
+//       className={cn("grid lg:gap-0 gap-5 items-center justify-items-center", {
+//         "lg:grid-cols-2": categories && categories?.length > 1,
+//         "w-1/2 mx-auto": categories && categories.length === 1,
+//       })}
+//     >
+//       {categories?.map((card) => <PriceCard key={card.id} card={card} />)}
+//     </div>
+//   );
+// };
