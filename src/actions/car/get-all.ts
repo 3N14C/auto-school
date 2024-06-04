bd5538cf2f808289aa8c7122cc869108b223a23f
3N14C <3n14c@vk.com>
2024-06-05 00:46:56 +0800

    Update get-all.ts

--- src/actions/car/get-all.ts
+++ src/actions/car/get-all.ts
@@ -1,14 +1,18 @@
 import { axiosInstance } from "@/configs/axois-config";
+import { baseUrl } from "@/configs/fetch-config";
 import { Car } from "@prisma/client";
 
 export const getAllCars = async () => {
-  const resposne = await axiosInstance.get<Car[]>("car/get-all", {
+  const resposne = await fetch(`${baseUrl}/car/get-all`, {
+    method: "GET",
     headers: {
-      "Cache-Control": "no-cache",
-      Pragma: "no-cache",
-      Expires: "0",
+      "Content-Type": "application/json",
     },
+    next: {
+      revalidate: 0,
+    },
+    cache: "no-store",
   });
 
-  return resposne.data;
+  return resposne.json() as Promise<Car[]>;
 };
